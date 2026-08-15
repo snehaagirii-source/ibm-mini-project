ac89c904b2ca5d3a145ef3a2ac5a1718033be850
Sneha Giri <snehaagirii@gmail.com>
2026-08-15 21:11:59 +0530

    Change MONGO_URI to secure connection string
    Updated MONGO_URI to use a secure connection string.

--- app.py
+++ app.py
@@ -13,10 +13,7 @@ app.secret_key = "localfix-secret-key"
 # MONGODB CONNECTION
 # --------------------------------
 
-MONGO_URI = os.getenv(
-    "MONGO_URI",
-    "mongodb://localhost:27017/"
-)
+MONGO_URI = "mongodb+srv://souravsharma7718_db_user:Dy2Z9ZJzYtkwWECR@cluster0.ljpb4nn.mongodb.net/?appName=Cluster0"
 
 client = MongoClient(MONGO_URI)
 
